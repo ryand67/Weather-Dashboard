8b71a706adb9210f649db35f20f3387a461a5695
ryand67 <rwdevenney@gmail.com>
2020-05-11 23:34:52 -0400

    JS setup to start work

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,29 +1,35 @@
 $(document).ready(function() {
+    //Grabbing parts of the page
     var mainTitle = $("#mainTitle");
+    var currentCityTitle = $("#currentCity");
+    var currentDateTitle = $("#currentDate");
     var currentTemp = $("#currentTemp");
     var currentHum = $("#currentHum");
     var currentWS = $("#currentWS");
     var currentUV = $("#currentUV");
     var searchInput = $("#searchInput")
+    var fiveDayDiv = $("#five-day-div");
 
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7e6a0f7a8f2fee7813d7ade6f80a831b",
-        Method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    })
+    
 
+    //Search button updates the weather
     $("#searchButton").on("click", function() {
         event.preventDefault();
-        console.log($("#searchInput").text());
-        updateWeather($("#searchInput").html());
+        updateWeather($("#searchInput").val());
     })
 
-    function updateWeather(place) {
-        console.log(place);
-    }
-
+    //Binds the popular cities list to call the function that updates the weather
     $("td").on("click", function() {
         updateWeather($(this).text());
     })
+
+    function updateWeather(place) {
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7e6a0f7a8f2fee7813d7ade6f80a831b",
+            Method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        })
+        console.log(place);
+    }
 })
