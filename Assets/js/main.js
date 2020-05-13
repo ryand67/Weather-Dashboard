cd3f5db043fd75986551011e3062ab832173abab
ryand67 <rwdevenney@gmail.com>
2020-05-12 21:03:38 -0400

    Search history functional

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     var currentUV = $("#currentUV");
     var searchInput = $("#searchInput")
     var fiveDayDiv = $("#five-day-div");
+    var searchHistoryList = $("#searchHistoryList");
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
     //Passes the place into a function that will call the api and then update the different parts of the forecast and creates the 5 day forecast boxes
@@ -35,17 +36,53 @@ $(document).ready(function() {
             currentWS.text(response.wind.speed + "MPH");
             //Sets UV index and color
 
+            fiveDayRender(place);
+        })
+    }
+
+    //Function that renders the five day forecast blocks
+    function fiveDayRender(place) {
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + place + '&units=imperial&appid=7e6a0f7a8f2fee7813d7ade6f80a831b',
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
         })
     }
 
     function renderHistory() {
-        
+        //Clears the list space to avoid repeats
+        searchHistoryList.html("");
+        //Runs through the history array one by one
+        searchHistory.forEach(function(e) {
+            //Creates a new li and hr to add to the master list and appends to the appropriate div
+            var newLI = $("<li>").text(e);
+            var newHR = $("<hr>");
+            searchHistoryList.prepend(newLI, newHR);
+        })
     }
 
+    //Adds the entry to the search history array
     function updateHistory(place) {
-        var placeVar = place.charAt(0).toUpperCase() + place.toLowerCase().slice(1);
+        //Makes sure the first character is capitalized and the rest is lowercase even if it's multiple words (eg LA, SF, etc)
+        var placeVar = "";
+        //Breaks up the city name by spaces
+        place = place.split(" ");
+        //Runs through each one of those, capitalizes the first letter, lowercases the rest, adds a space for the second word
+        place.forEach(function(e) {
+            placeVar += e.charAt(0).toUpperCase() + e.toLowerCase().slice(1) + " ";
+        })
+        //Gets rid of the extra space if it was only one word
+        placeVar = placeVar.trim();
+        //Adds the entry to the array
         searchHistory.push(placeVar);
+        //If it's longer than 5 start getting rid of the other entries
+        if(searchHistory.length >= 6) {
+            searchHistory.shift();
+        }
+        //Update the localStorage of the search history
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        //Render the search history
         renderHistory();
     }
 
